98ca83d1b45acd459753172c3acbe5488f4bd4b0
aoyshi <37639117+aoyshi@users.noreply.github.com>
2021-02-13 00:08:06 -0500

    linted project and configured linter

--- src/index.js
+++ src/index.js
@@ -11,5 +11,5 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 7000;
 
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}...`)
+  console.log(`Server listening on port ${PORT}...`);
 });
