a872e562fd657e137d082924adac36ed87787447
aoyshi <37639117+aoyshi@users.noreply.github.com>
2021-02-13 19:45:24 -0500

    changed route naming and ran linter

--- src/api/user/controller.js
+++ src/api/user/controller.js
@@ -2,12 +2,11 @@ const createUser = async (req, res) => {
   try {
     console.log(req.body);
     res.send('User route from controller');
-
-  } catch(err) {
+  } catch (err) {
     res.send(err);
   }
 };
 
 module.exports = {
-  createUser, 
+  createUser,
 };
