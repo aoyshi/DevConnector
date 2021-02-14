7284fd7c1bcd08fee60477618d059a4c35796e17
aoyshi <37639117+aoyshi@users.noreply.github.com>
2021-02-14 16:36:54 -0500

    extract only msg from user errors

--- src/api/user/validator.js
+++ src/api/user/validator.js
@@ -37,7 +37,7 @@ const validate = (req, res, next) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array().map(({ param, msg }) => ({ param, msg })) });
+    return res.status(400).json({ errors: errors.array().map(({ msg }) => ({ msg })) });
   }
   return next();
 };
